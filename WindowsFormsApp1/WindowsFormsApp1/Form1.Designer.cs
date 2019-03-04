c9d59737b09968566215f53caacfa6aa08e3464b
Opscursaw <45033463+Opscursaw@users.noreply.github.com>
2019-03-04 17:39:24 +0100

    update magueule (faut toujours que je créer des truc)

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -176,7 +176,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(231, 298);
+            this.ClientSize = new System.Drawing.Size(232, 316);
             this.Controls.Add(this.buttonP);
             this.Controls.Add(this.button0);
             this.Controls.Add(this.buttonA);
