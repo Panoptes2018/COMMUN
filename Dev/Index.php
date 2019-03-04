72e7755a0f72277877bfc07a7c6b2b7d9b0e30b5
Jiji83 <44972022+Jiji83@users.noreply.github.com>
2019-03-04 16:47:58 +0100

    Modif

--- Dev/Index.php
+++ Dev/Index.php
@@ -1,11 +1,27 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>Connexion Work on the road</title>
+    <title>Connexion Work on the road</title>
 </head>
+
 <body>
-        <h1>Connexion à Work in the road</h1>
-        Utilisateur :<input type="email" name="email"><br>
-        Mot de passe :<input type="password" name="Password"><br>
+    <form>
+        <center>
+            <h1>Connexion à Work in the road</h1>
+            <table>
+                <tr>
+                    <td>Utilisateur :</td>
+                    <td><input type="email" name="email"></td>
+                </tr>
+                <tr>
+                    <td>Mot de passe :</td>
+                    <td><input type="password" name="Password"></td>
+                </tr>
+            </table>
+        </center>
+    </form>
+    <input type="submit" name="Envoyer">
 </body>
+
 </html>
