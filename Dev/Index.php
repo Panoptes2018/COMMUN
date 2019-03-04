3a0de9bf4fb0f093eca9654b53585fa5666bee45
Jiji83 <44972022+Jiji83@users.noreply.github.com>
2019-03-04 15:42:10 +0100

    Update Index.php

---
+++ Dev/Index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Connexion Work on the road</title>
+</head>
+<body>
+        <h1>Connexion à Work in the road</h1>
+        Utilisateur :<input type="email" name="email"><br>
+        Mot de passe :<input type="password" name="Password"><br>
+</body>
+</html>
