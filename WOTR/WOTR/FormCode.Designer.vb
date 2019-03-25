2304910b9296d994411880e23fe006ff82140800
Opscursaw <45033463+Opscursaw@users.noreply.github.com>
2019-03-25 15:21:58 +0100

    Ajout de logi + version

---
+++ WOTR/WOTR/FormCode.Designer.vb
@@ -0,0 +1,196 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormCode
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.buttonP = New System.Windows.Forms.Button()
+        Me.button0 = New System.Windows.Forms.Button()
+        Me.buttonA = New System.Windows.Forms.Button()
+        Me.button9 = New System.Windows.Forms.Button()
+        Me.button8 = New System.Windows.Forms.Button()
+        Me.button7 = New System.Windows.Forms.Button()
+        Me.button6 = New System.Windows.Forms.Button()
+        Me.button5 = New System.Windows.Forms.Button()
+        Me.button4 = New System.Windows.Forms.Button()
+        Me.button3 = New System.Windows.Forms.Button()
+        Me.button2 = New System.Windows.Forms.Button()
+        Me.button1 = New System.Windows.Forms.Button()
+        Me.textBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'buttonP
+        '
+        Me.buttonP.Location = New System.Drawing.Point(154, 252)
+        Me.buttonP.Name = "buttonP"
+        Me.buttonP.Size = New System.Drawing.Size(63, 58)
+        Me.buttonP.TabIndex = 75
+        Me.buttonP.Text = "P"
+        Me.buttonP.UseVisualStyleBackColor = True
+        '
+        'button0
+        '
+        Me.button0.Location = New System.Drawing.Point(85, 252)
+        Me.button0.Name = "button0"
+        Me.button0.Size = New System.Drawing.Size(63, 58)
+        Me.button0.TabIndex = 74
+        Me.button0.Text = "0"
+        Me.button0.UseVisualStyleBackColor = True
+        '
+        'buttonA
+        '
+        Me.buttonA.Location = New System.Drawing.Point(16, 252)
+        Me.buttonA.Name = "buttonA"
+        Me.buttonA.Size = New System.Drawing.Size(63, 58)
+        Me.buttonA.TabIndex = 73
+        Me.buttonA.Text = "A"
+        Me.buttonA.UseVisualStyleBackColor = True
+        '
+        'button9
+        '
+        Me.button9.Location = New System.Drawing.Point(154, 188)
+        Me.button9.Name = "button9"
+        Me.button9.Size = New System.Drawing.Size(63, 58)
+        Me.button9.TabIndex = 72
+        Me.button9.Text = "9"
+        Me.button9.UseVisualStyleBackColor = True
+        '
+        'button8
+        '
+        Me.button8.Location = New System.Drawing.Point(85, 188)
+        Me.button8.Name = "button8"
+        Me.button8.Size = New System.Drawing.Size(63, 58)
+        Me.button8.TabIndex = 71
+        Me.button8.Text = "8"
+        Me.button8.UseVisualStyleBackColor = True
+        '
+        'button7
+        '
+        Me.button7.Location = New System.Drawing.Point(16, 188)
+        Me.button7.Name = "button7"
+        Me.button7.Size = New System.Drawing.Size(63, 58)
+        Me.button7.TabIndex = 70
+        Me.button7.Text = "7"
+        Me.button7.UseVisualStyleBackColor = True
+        '
+        'button6
+        '
+        Me.button6.Location = New System.Drawing.Point(154, 124)
+        Me.button6.Name = "button6"
+        Me.button6.Size = New System.Drawing.Size(63, 58)
+        Me.button6.TabIndex = 69
+        Me.button6.Text = "6"
+        Me.button6.UseVisualStyleBackColor = True
+        '
+        'button5
+        '
+        Me.button5.Location = New System.Drawing.Point(85, 124)
+        Me.button5.Name = "button5"
+        Me.button5.Size = New System.Drawing.Size(63, 58)
+        Me.button5.TabIndex = 68
+        Me.button5.Text = "5"
+        Me.button5.UseVisualStyleBackColor = True
+        '
+        'button4
+        '
+        Me.button4.Location = New System.Drawing.Point(16, 124)
+        Me.button4.Name = "button4"
+        Me.button4.Size = New System.Drawing.Size(63, 58)
+        Me.button4.TabIndex = 67
+        Me.button4.Text = "4"
+        Me.button4.UseVisualStyleBackColor = True
+        '
+        'button3
+        '
+        Me.button3.Location = New System.Drawing.Point(154, 60)
+        Me.button3.Name = "button3"
+        Me.button3.Size = New System.Drawing.Size(63, 58)
+        Me.button3.TabIndex = 66
+        Me.button3.Text = "3"
+        Me.button3.UseVisualStyleBackColor = True
+        '
+        'button2
+        '
+        Me.button2.Location = New System.Drawing.Point(85, 60)
+        Me.button2.Name = "button2"
+        Me.button2.Size = New System.Drawing.Size(63, 58)
+        Me.button2.TabIndex = 65
+        Me.button2.Text = "2"
+        Me.button2.UseVisualStyleBackColor = True
+        '
+        'button1
+        '
+        Me.button1.Location = New System.Drawing.Point(16, 60)
+        Me.button1.Name = "button1"
+        Me.button1.Size = New System.Drawing.Size(63, 58)
+        Me.button1.TabIndex = 64
+        Me.button1.Text = "1"
+        Me.button1.UseVisualStyleBackColor = True
+        '
+        'textBox1
+        '
+        Me.textBox1.Enabled = False
+        Me.textBox1.Location = New System.Drawing.Point(17, 31)
+        Me.textBox1.Name = "textBox1"
+        Me.textBox1.PasswordChar = Global.Microsoft.VisualBasic.ChrW(9632)
+        Me.textBox1.Size = New System.Drawing.Size(199, 20)
+        Me.textBox1.TabIndex = 63
+        Me.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'FormCode
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(233, 340)
+        Me.Controls.Add(Me.buttonP)
+        Me.Controls.Add(Me.button0)
+        Me.Controls.Add(Me.buttonA)
+        Me.Controls.Add(Me.button9)
+        Me.Controls.Add(Me.button8)
+        Me.Controls.Add(Me.button7)
+        Me.Controls.Add(Me.button6)
+        Me.Controls.Add(Me.button5)
+        Me.Controls.Add(Me.button4)
+        Me.Controls.Add(Me.button3)
+        Me.Controls.Add(Me.button2)
+        Me.Controls.Add(Me.button1)
+        Me.Controls.Add(Me.textBox1)
+        Me.Name = "FormCode"
+        Me.Text = "Formulaire Code"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Private WithEvents buttonP As Button
+    Private WithEvents button0 As Button
+    Private WithEvents buttonA As Button
+    Private WithEvents button9 As Button
+    Private WithEvents button8 As Button
+    Private WithEvents button7 As Button
+    Private WithEvents button6 As Button
+    Private WithEvents button5 As Button
+    Private WithEvents button4 As Button
+    Private WithEvents button3 As Button
+    Private WithEvents button2 As Button
+    Private WithEvents button1 As Button
+    Private WithEvents textBox1 As TextBox
+End Class
---
+++ WOTR/WOTR/FormConnection.Designer.vb
@@ -0,0 +1,153 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormConnection
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.buttonAnnuler = New System.Windows.Forms.Button()
+        Me.Valider = New System.Windows.Forms.Button()
+        Me.label4 = New System.Windows.Forms.Label()
+        Me.label3 = New System.Windows.Forms.Label()
+        Me.label2 = New System.Windows.Forms.Label()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.textBoxCode = New System.Windows.Forms.TextBox()
+        Me.textBoxMail = New System.Windows.Forms.TextBox()
+        Me.textBoxMDP = New System.Windows.Forms.TextBox()
+        Me.textBoxID = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'buttonAnnuler
+        '
+        Me.buttonAnnuler.Location = New System.Drawing.Point(234, 124)
+        Me.buttonAnnuler.Name = "buttonAnnuler"
+        Me.buttonAnnuler.Size = New System.Drawing.Size(170, 23)
+        Me.buttonAnnuler.TabIndex = 39
+        Me.buttonAnnuler.Text = "Annuler"
+        Me.buttonAnnuler.UseVisualStyleBackColor = True
+        '
+        'Valider
+        '
+        Me.Valider.Location = New System.Drawing.Point(19, 124)
+        Me.Valider.Name = "Valider"
+        Me.Valider.Size = New System.Drawing.Size(174, 23)
+        Me.Valider.TabIndex = 38
+        Me.Valider.Text = "Valider"
+        Me.Valider.UseVisualStyleBackColor = True
+        '
+        'label4
+        '
+        Me.label4.AutoSize = True
+        Me.label4.Location = New System.Drawing.Point(231, 73)
+        Me.label4.Name = "label4"
+        Me.label4.Size = New System.Drawing.Size(72, 13)
+        Me.label4.TabIndex = 37
+        Me.label4.Text = "Code d'acces"
+        '
+        'label3
+        '
+        Me.label3.AutoSize = True
+        Me.label3.Location = New System.Drawing.Point(16, 69)
+        Me.label3.Name = "label3"
+        Me.label3.Size = New System.Drawing.Size(71, 13)
+        Me.label3.TabIndex = 36
+        Me.label3.Text = "Mot de passe"
+        '
+        'label2
+        '
+        Me.label2.AutoSize = True
+        Me.label2.Location = New System.Drawing.Point(231, 18)
+        Me.label2.Name = "label2"
+        Me.label2.Size = New System.Drawing.Size(67, 13)
+        Me.label2.TabIndex = 35
+        Me.label2.Text = "Adresse Mail"
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Location = New System.Drawing.Point(16, 18)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(53, 13)
+        Me.label1.TabIndex = 34
+        Me.label1.Text = "Identifiant"
+        '
+        'textBoxCode
+        '
+        Me.textBoxCode.Location = New System.Drawing.Point(304, 66)
+        Me.textBoxCode.Name = "textBoxCode"
+        Me.textBoxCode.PasswordChar = Global.Microsoft.VisualBasic.ChrW(9632)
+        Me.textBoxCode.Size = New System.Drawing.Size(100, 20)
+        Me.textBoxCode.TabIndex = 33
+        '
+        'textBoxMail
+        '
+        Me.textBoxMail.Location = New System.Drawing.Point(304, 15)
+        Me.textBoxMail.Name = "textBoxMail"
+        Me.textBoxMail.Size = New System.Drawing.Size(100, 20)
+        Me.textBoxMail.TabIndex = 32
+        '
+        'textBoxMDP
+        '
+        Me.textBoxMDP.Location = New System.Drawing.Point(93, 66)
+        Me.textBoxMDP.Name = "textBoxMDP"
+        Me.textBoxMDP.PasswordChar = Global.Microsoft.VisualBasic.ChrW(9632)
+        Me.textBoxMDP.Size = New System.Drawing.Size(100, 20)
+        Me.textBoxMDP.TabIndex = 31
+        '
+        'textBoxID
+        '
+        Me.textBoxID.Location = New System.Drawing.Point(93, 18)
+        Me.textBoxID.Name = "textBoxID"
+        Me.textBoxID.Size = New System.Drawing.Size(100, 20)
+        Me.textBoxID.TabIndex = 30
+        '
+        'FormConnection
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(421, 163)
+        Me.Controls.Add(Me.buttonAnnuler)
+        Me.Controls.Add(Me.Valider)
+        Me.Controls.Add(Me.label4)
+        Me.Controls.Add(Me.label3)
+        Me.Controls.Add(Me.label2)
+        Me.Controls.Add(Me.label1)
+        Me.Controls.Add(Me.textBoxCode)
+        Me.Controls.Add(Me.textBoxMail)
+        Me.Controls.Add(Me.textBoxMDP)
+        Me.Controls.Add(Me.textBoxID)
+        Me.Name = "FormConnection"
+        Me.Text = "Formulaire de Connexion"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Private WithEvents buttonAnnuler As Button
+    Private WithEvents Valider As Button
+    Private WithEvents label4 As Label
+    Private WithEvents label3 As Label
+    Private WithEvents label2 As Label
+    Private WithEvents label1 As Label
+    Private WithEvents textBoxCode As TextBox
+    Private WithEvents textBoxMail As TextBox
+    Private WithEvents textBoxMDP As TextBox
+    Private WithEvents textBoxID As TextBox
+End Class
